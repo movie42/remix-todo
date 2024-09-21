8b9823f801126a63a97b45d2c6c6a07457e99848
HYUNSU <movie3342@gmail.com>
2024-09-21 14:15:22 +0900

    feat: learn base supabase and remix loader - auth

--- app/root.tsx
+++ app/root.tsx
@@ -1,25 +1,39 @@
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
 import {
+  json,
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
+  useRevalidator
 } from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node";
 
+import { createBrowserClient } from "@supabase/ssr";
+import { SupabaseClient } from "@supabase/supabase-js";
+import type { Database } from "db_types";
+import { useEffect, useState } from "react";
+import createServerSupabase from "utils/supabase.server";
 import "./tailwind.css";
+
+type TypedSupabseClient = SupabaseClient<Database>;
+
+export type SupabaseOutletContext = {
+  supabase: TypedSupabseClient;
+};
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
     rel: "preconnect",
     href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
+    crossOrigin: "anonymous"
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
+    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+  }
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
@@ -32,7 +46,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        {children}
+        <main>{children}</main>
         <ScrollRestoration />
         <Scripts />
       </body>
@@ -40,6 +54,41 @@ export function Layout({ children }: { children: React.ReactNode }) {
   );
 }
 
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const env = {
+    SUPABASE_URL: process.env.SUPABASE_URL!,
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY!
+  };
+
+  const supabase = createServerSupabase({ request });
+
+  const {
+    data: { session }
+  } = await supabase.auth.getSession();
+
+  return json({ env, session });
+};
+
 export default function App() {
-  return <Outlet />;
+  const { env, session } = useLoaderData<typeof loader>();
+  const revalidator = useRevalidator();
+  const [supabase] = useState(() =>
+    createBrowserClient<Database>(env.SUPABASE_URL, env.SUPABASE_ANON_KEY)
+  );
+  const serverAccessToken = session?.access_token;
+
+  useEffect(() => {
+    const {
+      data: { subscription }
+    } = supabase.auth.onAuthStateChange((_, session) => {
+      if (session?.access_token !== serverAccessToken) {
+        revalidator.revalidate();
+      }
+    });
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [supabase, serverAccessToken, revalidator]);
+
+  return <Outlet context={{ supabase }} />;
 }
---
+++ utils/supabase.server.ts
@@ -0,0 +1,30 @@
+import {
+  createServerClient,
+  parseCookieHeader,
+  serializeCookieHeader
+} from "@supabase/ssr";
+import type { Database } from "db_types";
+
+export default ({ request }: { request: Request }) => {
+  const headers = new Headers();
+
+  return createServerClient<Database>(
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return parseCookieHeader(request.headers.get("Cookie") ?? "");
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) =>
+            headers.append(
+              "Set-Cookie",
+              serializeCookieHeader(name, value, options)
+            )
+          );
+        }
+      }
+    }
+  );
+};
